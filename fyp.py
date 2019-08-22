22c920f5901ce0b614358dd07450c71b9e39315f
Zheng Wei <zhengwei997@gmail.com>
2019-08-22 17:07:33 +0800

    fixed from to field validation

--- fyp.py
+++ fyp.py
@@ -10,6 +10,7 @@ import numpy as np
 import serial as serial
 import time
 import cv2
+import threading
 
 
 app = QApplication([])
@@ -24,6 +25,7 @@ window.setFixedSize(1131, 900)
 # WARNING!!!: max value of last wave range needs to be adjusted if sample rate is modified
 SAMPLE_RATE = 50
 SAMPLE_INTERVAL = 1.0/SAMPLE_RATE
+FFT_UPDATE_RATE = 10
 
 #WARNING!!!: Frame cutpoint values change from video to video
 DELTA_FRAMES = (0,268)
@@ -46,6 +48,9 @@ window.timer = None
 window.classCheckTimer = None
 window.waveClass = None
 window.cap = None
+window.fetchTimer = None
+window.fetch_thread = None
+window.training_thread = None
 window.temp1 = False
 
 window.waveCounter = []
@@ -77,7 +82,18 @@ window.layoutPlot.addWidget(window.canvas)
 window.toolbar = NavigationToolbar(window.canvas, window.wgtPlot, coordinates=True)
 window.layoutPlot.addWidget(window.toolbar)         
 
-    
+def load_from_file():
+    successful_load = load_csv()
+    if successful_load:
+        clear_wave_counter()
+        fft_segments = range(SAMPLE_RATE, len(window.ydata), FFT_UPDATE_RATE)
+        for segment in fft_segments:
+            x, y, peak_freq = calc_current_fft(segment)
+            increase_wave_counter(peak_freq)
+        plot_amp()
+        plot_histogram()
+
+
 def load_csv():
     filename = QFileDialog.getOpenFileName(window, 'Open File')
     filename = str(filename[0])
@@ -90,26 +106,25 @@ def load_csv():
                 line = line[:-2]
                 temp = float(line).__round__(4)
                 window.ydata = np.append(window.ydata, [temp])
-            
+
+        window.xdata = np.arange(0, len(window.ydata)/SAMPLE_RATE, SAMPLE_INTERVAL)   
+        print(window.xdata[:50]) 
         print("data loaded")
-        plot_amp()
+        return True
     else:
         window.lblLoadError.setText("Error: the file chosen is not a .csv file")
         window.lblLoadError.setHidden(False)
+        return False
 
 
 def plot_amp():
     window.ax1.clear()
     window.ax1.set_title("time-amplitude")
-    window.ax1.set_xlim(left=0, right=20)
-    window.xdata = np.arange(0, len(window.ydata)/SAMPLE_RATE, SAMPLE_INTERVAL)
-    # window.xdata = np.linspace(0, 10, 50)
-    # window.ydata = (np.cos(3*window.xdata))
+    # window.ax1.set_xlim(left=0, right=20)
+    # window.xdata = np.arange(0, len(window.ydata)/SAMPLE_RATE, SAMPLE_INTERVAL)
     start = time.time()
     print("plotting graph...")
-    # window.ax1.plot(X, Y)
     window.ax1.plot(window.xdata, window.ydata)
-    plot_fft()
     window.canvas.draw()
     end = time.time()
     print(end-start)
@@ -133,12 +148,20 @@ def load_raw():
             window.ydata = np.array([])
             discard = window.ser.readline()
 
+            # fetch_thread = threading.Thread(target=loop_fetch, daemon=True)
+            # window.fetch_thread = Fetch_Thread()
+            # window.fetch_thread.start()
             window.fetchTimer = QtCore.QTimer(window)
             window.fetchTimer.timeout.connect(fetch_raw)
             window.fetchTimer.start(20)
+
             window.drawTimer = QtCore.QTimer(window)
             window.drawTimer.timeout.connect(roll_live)
-            window.drawTimer.start(1000)
+            window.drawTimer.start(200)
+
+            # training_thread = threading.Thread(target=start_train, daemon=True)
+            # window.training_thread = Training_Thread()
+            # window.training_thread.start()
             window.classCheckTimer = QtCore.QTimer(window)
             window.classCheckTimer.setSingleShot(True)
             window.classCheckTimer.timeout.connect(video_init)
@@ -159,10 +182,16 @@ def load_raw():
         disable_input(window.isLoading)
         window.btnRoll.setText("Roll")
 
-        
+
+def loop_fetch():
+    window.fetchTimer = QtCore.QTimer(window)
+    window.fetchTimer.timeout.connect(fetch_raw)
+    window.fetchTimer.start(20)
+
+
 def fetch_raw():
     if window.temp1 == False:
-        print(int(QtCore.QThread.currentThreadId()))
+        # print(int(QtCore.QThread.currentThreadId()))
         window.temp1 = True
     # start = time.time()
     try:
@@ -182,8 +211,9 @@ def fetch_raw():
         window.fetchTimer.stop()
         window.drawTimer.stop()
         window.isLoading = False
-        window.fetchTimer = None
+        # window.fetch_thread.fetchTimer = None
         window.drawTimer = None
+        # window.training_thread.classCheckTimer = None
         window.waveClass = None
         window.lblLoadError.setText("external device disconnected")
         window.lblLoadError.setHidden(False)
@@ -191,6 +221,14 @@ def fetch_raw():
         disable_input(window.isLoading)
     # end = time.time()
     # print(end-start)
+
+
+def start_train():
+    window.classCheckTimer = QtCore.QTimer(window)
+    window.classCheckTimer.setSingleShot(True)
+    window.classCheckTimer.timeout.connect(video_init)
+    window.classCheckTimer.start(2000)
+
 
 def roll_live():
     x = len(window.ydata) * 0.02
@@ -203,36 +241,51 @@ def roll_live():
     else:
         window.ax1.set_xlim(left=0, right=max_width)
     
-    print(len(window.xdata))
+    # print(len(window.xdata))
     window.ax1.plot(window.xdata, window.ydata)
-    plot_fft()
+    if (len(window.ydata) >= 50):
+        x, y, peak = calc_current_fft(len(window.ydata) - 1)
+        increase_wave_counter(peak)
+        plot_fft(x, y)
+        plot_histogram()
     window.canvas.draw()
 
 
-def plot_fft():
-    if len(window.ydata) >= SAMPLE_RATE:
-            sample = window.ydata[-1 * SAMPLE_RATE:]
-            adjusted_sample = np.subtract(sample, np.average(sample))
-            fftY = np.fft.fft(adjusted_sample)
-            fftY = 2.0/SAMPLE_RATE * np.abs(fftY[:int(SAMPLE_RATE/2)])
-            fftX = np.linspace(0, int(SAMPLE_RATE/2), int(SAMPLE_RATE/2))
-            brain_freq_index = np.where(fftY == np.amax(fftY))[0][0]
-            brainFreq = fftX[brain_freq_index]
+def calc_current_fft(endpoint):
+    sample = window.ydata[(endpoint - SAMPLE_RATE):(endpoint + 1)]
+    adjusted_sample = np.subtract(sample, np.average(sample))
+    fftY = np.fft.fft(adjusted_sample)
+    fftY = 2.0/SAMPLE_RATE * np.abs(fftY[:int(SAMPLE_RATE/2)])
+    fftX = np.linspace(0, int(SAMPLE_RATE/2), int(SAMPLE_RATE/2))
+    peak_index = np.where(fftY == np.amax(fftY))[0][0]
+    peak_freq = fftX[peak_index]
+    return (fftX, fftY, peak_freq)
 
-            fftColor = None
 
-            window.waveClass = next((wave for wave in WAVE_RANGES if brainFreq > wave["min"] and brainFreq <= wave["max"]), None)
-            if window.waveClass is not None:
-                fftColor = window.waveClass["color"]
-                index_to_increase = (next(window.waveCounter[0].index(wave) for wave in window.waveCounter[0] if wave == window.waveClass["name"]))
-                window.waveCounter[1][index_to_increase] += 0.2
-                plot_histogram()
-            else:
-                fftColor = "k"
-            
-            window.ax2.clear()
-            window.ax2.set_title("fft")
-            window.ax2.plot(fftX, fftY, color=fftColor)
+def clear_wave_counter():
+    for count in range(len(window.waveCounter[1])):
+        window.waveCounter[1][count] = 0.0
+
+
+def increase_wave_counter(freq):
+    color = "k"
+
+    window.waveClass = next((wave for wave in WAVE_RANGES if freq > wave["min"] and freq <= wave["max"]), None)
+    if window.waveClass is not None:
+        index_to_increase = (next(window.waveCounter[0].index(wave) for wave in window.waveCounter[0] if wave == window.waveClass["name"]))
+        window.waveCounter[1][index_to_increase] += 0.2
+
+
+def plot_fft(x, y):
+    fftColor = None
+    try:
+        fftColor = window.waveClass["color"]
+    except:
+        fftColor = "k"
+    
+    window.ax2.clear()
+    window.ax2.set_title("fft")
+    window.ax2.plot(x, y, color=fftColor)
 
 
 def plot_histogram():
@@ -241,6 +294,7 @@ def plot_histogram():
     x = window.waveCounter[0]
     y = window.waveCounter[1]
     window.ax3.bar(x, y, color=window.waveCounter[2])
+    window.canvas.draw()
 
 
 def video_breakdown():
@@ -262,11 +316,13 @@ def video_init():
         window.cap = cv2.VideoCapture(VIDEO_NAME)
         cv2.namedWindow('azalea')
         load_frames()
+    else:
+        print(hasClass)
 
 
 def load_frames():
     current_frame = window.waveClass["frames"][0]
-    print(current_frame)
+    # print(current_frame)
     while window.waveClass is not None:
         counter = 0
         start_frame = window.waveClass["frames"][0]
@@ -293,19 +349,61 @@ def load_frames():
     video_breakdown()
 
 
-def save_file():
-    fileName = QFileDialog.getSaveFileName(window, 'Save File')
+def validate_from_to(from_val, to_val):
+    save = False
+    left = None
+    right = None
 
-    if fileName != '':
-        fileName = str(fileName[0]) + '.csv'
-        with open(fileName, "w+") as f:
-            try:
-                leftLim = int(window.tfFrom.text())
-                rightLim = int(window.tfTo.text())
-                np.savetxt(f, window.ydata[leftLim*50:rightLim*50], delimiter=',', header='number')
-            except:      
-                np.savetxt(f, window.ydata, delimiter=',', header='number')
-            print("saved")
+    if from_val == "" and to_val == "" and len(window.ydata) > 0:
+        save = True
+
+    else:
+        try:
+            leftLim = int(from_val)
+            rightLim = int(to_val)
+
+            if leftLim < 0 or rightLim < 0:
+                raise ValueError
+
+            elif leftLim >= rightLim:
+                raise Exception("To value must be larger than From value")
+
+            elif rightLim > 300:
+                raise Exception("To value has a limit of 300")
+
+            elif rightLim > window.xdata[-1]:
+                raise Exception("To value is over current data length")
+
+        except ValueError:
+            window.lblSaveError.setText("From and To values must be positive integers") 
+            window.lblSaveError.setHidden(False)
+
+        except Exception as error:
+            window.lblSaveError.setText(str(error))
+            window.lblSaveError.setHidden(False)
+
+        else:
+            save = True
+            left = leftLim
+            right = rightLim
+
+    return(save, left, right)
+
+
+def save_file():
+    save, left, right = validate_from_to(window.tfFrom.text(), window.tfTo.text())
+
+    if save == True:     
+        fileName = QFileDialog.getSaveFileName(window, 'Save File')
+
+        if fileName != '':
+            fileName = str(fileName[0]) + '.csv'
+            with open(fileName, "w+") as f:
+                if left != None:
+                    np.savetxt(f, window.ydata[left*50:right*50], delimiter=',', header='number')
+                else:
+                    np.savetxt(f, window.ydata, delimiter=',', header='number')
+                
             window.lblSaveError.setText("data saved")
             window.lblSaveError.setHidden(False)
 
@@ -318,18 +416,39 @@ def disable_input(a: bool):
 
 
 def save_preview():
-    try:
-        leftLim = int(window.tfFrom.text())
-        rightLim = int(window.tfTo.text())
+    save, left, right = validate_from_to(window.tfFrom.text(), window.tfTo.text())
 
-        window.ax1.set_xlim(left=leftLim, right=rightLim)
+    if save == True:
+        if left != None:
+            window.ax1.set_xlim(left=left, right=right)
+        else:
+            window.ax1.set_xlim(left=window.xdata[0], right=window.xdata[-1])
+
         window.canvas.draw()
-    except:
-        window.lblSaveError.setText("from and to values must be numbers")
-    window.lblSaveError.setHidden(False)
 
 
-window.btnLoad.clicked.connect(load_csv)
+class Fetch_Thread(QtCore.QThread):
+    fetchTimer = None
+    def run(self):
+        self.fetchTimer = QtCore.QTimer()
+        self.fetchTimer.timeout.connect(fetch_raw)
+        self.fetchTimer.start(20)
+        while self.fetchTimer != None:
+            time.sleep(0.02)
+
+
+class Training_Thread(QtCore.QThread):
+    classCheckTimer = None
+    def run(self):
+        self.classCheckTimer = QtCore.QTimer()
+        self.classCheckTimer.setSingleShot(True)
+        self.classCheckTimer.timeout.connect(video_init)
+        self.classCheckTimer.start(2000)
+        while self.classCheckTimer != None:
+            time.sleep(0.2)
+
+
+window.btnLoad.clicked.connect(load_from_file)
 window.btnSave.clicked.connect(save_file)
 window.btnRoll.clicked.connect(load_raw)
 window.btnSavePreview.clicked.connect(save_preview)
